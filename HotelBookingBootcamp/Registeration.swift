f172b7f89e7fe69293b1434d65a9b42869dd47aa
Meghs <ms8615@srmist.edu.in>
2024-01-11 19:18:09 +0530

    first commit

---
+++ HotelBookingBootcamp/Registeration.swift
@@ -0,0 +1,22 @@
+//
+//  Registeration.swift
+//  HotelBookingBootcamp
+//
+//  Created by user1 on 11/01/24.
+//
+
+import Foundation
+
+struct Registeration {
+    var firstName : String
+    var lastName : String
+    var emailAddress : String
+    
+    var checkInDate : Date
+    var checkOutDate : Date
+    var numberOfAdults : Int
+    var numberOfChildren : Int
+    
+    var wifi  : Bool
+    var roomType : RoomType
+}
